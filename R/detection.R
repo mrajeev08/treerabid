7918ca70838372c22bad0b0d6e45fefb0f833d28
Malavika Rajeev <mrajeev08@gmail.com>
2021-07-14 23:50:40 -0700

    with convolution

--- R/detection.R
+++ R/detection.R
@@ -16,256 +16,106 @@
 #' @param fixed_pars parameters to pass to the d functions (i.e. dempiric, dgamma, dgenetic, dspatial)
 #' @param alpha the value at which to cutoff the potential number of cases between two linked cases
 #'  see Cori et al. for more details
+#'  si_base <- distcrete::distcrete("lnorm", 1L,
+#'                                  meanlog = treerabid::params_treerabid$SI_meanlog,
+#'                                  sdlog = treerabid::params_treerabid$SI_sdlog)$d(1:1000)
 #'
-#' @return an optim object
-#' @export
-#'
-estimate_detection <- function(dist_pairs, p = NULL,
-                               pdf = c("temporal", "spatial", "empirical", "genetic"),
-                               fixed_pars, alpha = 0.001) {
+convolve_empirical <- function(p, alpha = 0.001, max_time, min_pi) {
 
-  # optim
-  out <- optim(par = runif(1, 0.1, 1),
-               est_detection,
-               x = dist_pairs,
-               p = p,
-               pdf = pdf,
-               fixed_pars = fixed_pars,
-               alpha = 0.001,
-               method = "Brent", upper = 1, lower = 0.1)
-
-  return(out)
-
-}
-
-#' Function to get likelihood of a detection probability given observed distances
-#' between case pairs and an underlying distribution
-#'
-#' @param x distances between case pairs
-#' @inheritParams estimate_detection
-#' @param par starting value for detection probability
-#' @param adj constraining distances to be non-zero
-#'
-#' @return
-#' @export
-#'
-est_detection <- function(x, p = NULL,
-                          alpha,
-                          pdf = c("temporal", "spatial", "empirical", "genetic"),
-                          fixed_pars = list(),
-                          par = c(pi = 0.1),
-                          adj = 1e-6) {
-
-  pi <- par
-  if(pdf == "temporal") {
-    x[x == 0] <- x[x == 0] + 1e-6
-    loglik <- -sum(dtemporal(x, shape = fixed_pars$SI_shape, scale = fixed_pars$SI_scale,
-                             pi = pi, alpha = alpha, log = TRUE), na.rm = TRUE)
-
-  }
-
-  if(pdf == "spatial") {
-    x[x == 0] <- x[x == 0] + 1e-6
-    loglik <- -sum(dspatial(x, sd = fixed_pars$sd, pi = pi, alpha = alpha, log = TRUE), na.rm = TRUE)
-
-  }
-
-  if(pdf == "genetic") {
-
-    loglik <- -sum(dgenetic(x, gamma_shape = fixed_pars$SI_shape,
-                            gamma_scale =  fixed_pars$SI_scale,
-                            poisson_rate = fixed_pars$snp_rate_daily,
-                            pi = pi, alpha = alpha, log = TRUE), na.rm = TRUE)
-
-  }
-
-  if(pdf == "empirical") {
-    if(is.null(p)) stop("empirical estimates require p (i.e. vector of probabilities)")
-    y <- dempiric(p, pi, alpha = 0.001)
-    maxl <- max(c(x, length(y)))
-    px <- tabulate(x, maxl)/sum(maxl)
-    py <- y/sum(y)
-    loglik <- kb_stat(x, y)
-  }
-
-  return(loglik)
-
-}
-
-dtemporal <- function(x, shape, rate = 1, scale = 1/rate, pi, alpha = 0.001, log = TRUE) {
-
-  max_kappa <- qgeom(1 - alpha, pi) + 1L
-  weights <- dgeom(seq_len(max_kappa) - 1, pi)
-  weights <- weights / sum(weights)
-  distributions <- convolve_gamma(shape, scale = scale,
-                                  kappa = max_kappa, keep_all = TRUE, log = log)(x)
-
-  out <- distributions %*% weights
-  return(as.vector(out))
-}
-
-dgenetic <- function(x, gamma_shape, gamma_rate = 1,
-                     gamma_scale = 1 / gamma_rate,
-                     poisson_rate,
-                     pi,
-                     alpha = 0.001, log = TRUE) {
-
-  max_kappa <- qgeom(1 - alpha, pi) + 1L
-  weights <- dgeom(seq_len(max_kappa) - 1, pi)
-  weights <- weights / sum(weights)
-  distributions <- convolve_gamma_poisson(gamma_shape,
-                                          gamma_scale = gamma_scale,
-                                          poisson_rate = poisson_rate,
-                                          kappa = max_kappa,
-                                          keep_all = TRUE,  log = log)(x)
-
-  out <- distributions %*% weights
-  return(as.vector(out))
-}
-
-dspatial <- function(x, sd, pi, alpha = 0.001, log = TRUE) {
-
-  max_kappa <- qgeom(1 - alpha, pi) + 1L
-  weights <- dgeom(seq_len(max_kappa) - 1, pi)
-  weights <- weights / sum(weights)
-  distributions <- convolve_spatial(sd = sd,
-                                    kappa = max_kappa,
-                                    keep_all = TRUE,
-                                    log = log)(x)
-
-  out <- distributions %*% weights
-  return(as.vector(out))
-}
-
-convolve_gamma <- function(shape, rate = 1, scale = 1 / rate,
-                           kappa, keep_all = FALSE,
-                           log = TRUE) {
-
-  if (keep_all) {
-    f <- function(x) {
-      out <- sapply(x, function(e)
-        stats::dgamma(e, shape = (1:kappa) * shape, scale = scale, log = log)
-      )
-
-      if (is.matrix(out)) {
-        return(t(out))
-      } else {
-        return(matrix(out))
-      }
-    }
-  }  else {
-    f <- function(x) stats::dgamma(x, shape = kappa * shape, scale = scale, log = log)
-  }
-  return(f)
-}
-
-
-convolve_gamma_poisson <- function(gamma_shape, gamma_rate = 1,
-                                   gamma_scale = 1 / gamma_rate,
-                                   poisson_rate, kappa, keep_all = FALSE,
-                                   log = TRUE) {
-
-  ## using prob = 1-p intead of p so that our definition correponds to that of dnbinom
-  prob <- 1 - (gamma_scale * (poisson_rate / (gamma_scale * poisson_rate + 1)))
-
-  if (keep_all) {
-    f <- function(x) {
-      out <- sapply(x, function(e)
-        stats::dnbinom(e, size = (1:kappa) * gamma_shape, prob = prob, log = log)
-      )
-
-      if (is.matrix(out)) {
-        return(t(out))
-      } else {
-        return(matrix(out))
-      }
-    }
-  } else {
-    f <- function(x)
-      stats::dnbinom(x, size = kappa * gamma_shape, prob = prob, log = log)
-  }
-  return(f)
-}
-
-convolve_spatial <- function(sd, kappa, keep_all = FALSE, log = TRUE) {
-
-  if (keep_all) {
-    f <- function(x) {
-      out <- sapply(x, function(e)
-        VGAM::drayleigh(e, scale=sd*sqrt(1:kappa), log = log))
-
-      if (is.matrix(out)) {
-        return(t(out))
-      } else {
-        return(matrix(out))
-      }
-
-    }
-  } else {
-    f <- function(x) VGAM::drayleigh(x,scale=sd*sqrt(kappa), log = log)
-  }
-  return(f)
-}
-
-dempiric <- function(p, pi, alpha = 0.001) {
-
-  max_kappa <- qgeom(1 - alpha, pi) + 1L
-  weights <- dgeom(seq_len(max_kappa) - 1, pi)
-  weights <- weights / sum(weights)
-  distributions <- convolve_empirical(p, max_kappa, TRUE)
-
-  out <- distributions %*% weights
-  return(as.vector(out))
-}
-
-
-convolve_empirical <- function(x, kappa, keep_all = FALSE) {
-
-  if (kappa == 1) {
-    if (keep_all) {
-      x <- matrix(x)
-      colnames(x) <- "1"
-    }
-    return(x)
-  }
-
-  if (keep_all) {
     out <- list()
-    out[[1]] <- x
+    p <- fill_with(p, 0, max_time)
+    out[[1]] <- p
+    max_kappa <- get_kappa(alpha, pi = min_pi)
 
-    for (k in 2:kappa) {
-      out[[k]] <- stats::convolve(out[[k-1]],
-                                  rev(x),
-                                  type="open")
+    for (k in 2:max_kappa) {
+      ps <- stats::convolve(out[[k-1]],
+                            rev(p),
+                            type="open")
+      if(length(ps) < max_time) {
+        ps <- fill_with(ps, 0, max_time)
+      }
+
+      if(length(ps) > max_time) {
+        ps <- ps[1:max_time]
+      }
+
+      if(sum(ps) > alpha) {
+        out[[k]] <- ps
+      } else {
+        break
+      }
     }
 
-    L <- length(out[[kappa]])
-    out <- lapply(out, fill_with, 0, L)
-    out <- as.matrix(data.frame(out))
+    out <- do.call(cbind, out)
+    out[out < 0] <- 0
     colnames(out) <- seq_len(ncol(out))
-  } else {
-    out <- x
 
-    for (k in 2:kappa) {
-      out <- stats::convolve(out,
-                             rev(x),
-                             type="open")
-    }
-  }
   return(out)
 }
+
+get_empirical_probs <- function(p_mat, t_diff) {
+
+  out_probs <- p_mat[t_diff, ]
+
+}
+
+# sim kappa between case dates  (instead of max_gens, set an alpha level!)
+est_generations <- function(out_probs,
+                            kappa_weights = TRUE) {
+
+   k_max <- ncol(out_probs)
+   scaled_probs <- out_probs/rowSums(out_probs)
+   gens <- apply(scaled_probs, 1, function(x) sample(k_max, 1, prob = x))
+
+   if(kappa_weights) {
+    gens <- tabulate(gens, nbins = k_max)/length(gens)
+  }
+
+  return(gens)
+}
+
+est_pi <- function(t_diff, nsims = 1000, out_probs,
+                   candidate_pis) {
+
+  sims <- lapply(seq_len(nsims),
+                 function(x) {
+                   est_generations(out_probs,
+                                   kappa_weights = TRUE)
+                             })
+
+  candidate_weights <- lapply(candidate_pis,
+                              function(x) {
+                                dgeom(seq_len(ncol(out_probs)) - 1, x)
+                              })
+  candidate_weights <- do.call(cbind, candidate_weights)
+  out <- unlist(lapply(sims, function(x) {
+    candidate_pis[which.min(colSums((x - candidate_weights)^2))]
+  }))
+
+  return(out)
+}
+
+# # get the prob mat
+# max_base <- qlnorm(0.999, meanlog = treerabid::params_treerabid$SI_meanlog,
+#                    sdlog = treerabid::params_treerabid$SI_sdlog)
+# p <- distcrete::distcrete("lnorm", 1L,
+#                            meanlog = treerabid::params_treerabid$SI_meanlog,
+#                            sdlog = treerabid::params_treerabid$SI_sdlog)$d(1:max_base)
+# out_mat <- convolve_empirical(p, alpha = 0.001, max_time = 15000, min_pi = 0.01)
+# system.time({
+#  tt <- rbindlist(lapply(seq(0.01, 0.99, by = 0.05), function(z) {
+#   rbindlist(lapply(seq_len(10), function(x) {
+#     t_diff <- sim_times_pi(si_fun, nobs = 572, params = treerabid::params_treerabid, alpha = 0.001,
+#                            pi = z)
+#     out_probs <- get_empirical_probs(out_mat, t_diff)
+#     ests <- est_pi(t_diff, nsims = 10, out_probs,
+#                    candidate_pis = seq(0.01, 0.99, by = 0.25))
+#     data.table(true = z, estimated = ests, sim = x)}))
+#   }))
+# })
 
 
 # Helpers ----
-
-# inputs should be scaled to 1
-kb_stat <- function(px, py) {
-
-  sum(exp(px) * (log(exp(px) / exp(py))))
-
-}
-
-
 fill_with <- function(x, filling, L = length(x)) {
   if (L <= length(x)) {
     return(x)
@@ -343,24 +193,30 @@ sim_times_pi <- function(si_fun, nobs, params, alpha = 0.001, pi) {
 }
 
 # fit N simulations to kappa and get minimum
-
 fit_sims_pi <- function(t_diff, nsims = 1000,
                         candidate_pis, si_fun, params, alpha = 0.001) {
 
   max_max_kappa <- get_kappa(alpha, pi = min(candidate_pis))
-  sims <- parallel::mclapply(seq_len(nsims),
-                             function(x) {
-                               sim_generations(t_diff, si_fun, params,
-                                               max_kappa = max_max_kappa,
-                                               kappa_weights = TRUE)
+
+  candidate_weights <- lapply(candidate_pis,
+                              function(x) {
+                                dgeom(seq_len(ncol(out_probs)) - 1, x)
                               })
-  unlist(parallel::mclapply(sims, function(x) {
-    ss <- unlist(lapply(candidate_pis, function(z) {
-        max_kappa <- get_kappa(alpha, pi = z)
-        fit_detection_from_kappa(alpha, weights_obs = x, pi = z)
-      }))
-    candidate_pis[which.min(ss)]
-  }))
+  candidate_weights <- do.call(cbind, candidate_weights)
+
+  out <-
+    parallel::mclapply(
+      seq_len(nsims),
+      function(x) {
+        weights_sim <- sim_generations(t_diff, si_fun, params,
+                                       max_kappa = max_max_kappa,
+                                       kappa_weights = TRUE)
+        candidate_pis[which.min(colSums((weights_sim - candidate_weights)^2))]
+      }
+    )
+
+    return(out)
+
 }
 
 get_kappa <- function(alpha, pi, min_kappa = 2) {
